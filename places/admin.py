d1bff9d47679f2f1eadb7300d65d400035b1686b
OrozomambetovaMeerim <meerimorozomambetova@gmail.com>
2021-07-30 16:37:31 +0600

    added place img

--- places/admin.py
+++ places/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.db.models import fields
 from .models import Place, Feedback
 
 
 admin.site.register(Place)
-admin.site.register(Feedback)
+
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ['text', 'place', 'user']  # 'checked'
+    list_editable = ['user']   #  'checked'
+    list_filter = ['place'] # 'checked'
+    search_fields = ['text', 'place__name', 'place__location', 'place__description']
+
+    fields = ['user', 'place', 'text']
+    readonly_fields = ['place', 'text']
+
+admin.site.register(Feedback, FeedbackAdmin)
--- places/models.py
+++ places/models.py
@@ -14,6 +14,7 @@ class Place(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     views_count = models.IntegerField(default=0)
     is_publicated = models.BooleanField(default=True)
+    img = models.ImageField(upload_to='places', null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -41,6 +42,8 @@ class Feedback(models.Model):
     )
 
     text = models.TextField(verbose_name='Текст обратной связи')
+    
+    chacked = models.BooleanField(default=False, verbose_name='Обработано')
 
     def __str__(self):
         return self.text[:20]
